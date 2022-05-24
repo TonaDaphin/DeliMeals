d4154c2b99869697e94450e651ba0f6096b38062
tona <tonadaphine77@gmail.com>
2022-05-24 12:09:49 +0200

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,73 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
+import './dummy_data.dart';
+import './screens/filters_screen.dart';
+import './screens/tabs_screen.dart';
+import './screens/meal_detail_screen.dart';
+import './screens/category_meals_screen.dart';
+import './screens/categories_screen.dart';
+import './models/meal.dart';
 
-void main() => runApp(DevicePreview(enabled: !kReleaseMode, builder: (context) => MyApp()));
+void main() =>
+    runApp(DevicePreview(enabled: kReleaseMode, builder: (context) => MyApp()));
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegetarian': false,
+    'vegan': false
+  };
+
+  List<Meal> _availableMeals = DUMMY_MEALS;
+  List<Meal> _favoriteMeals = [];
+
+  void _setFilters(Map<String, bool> filterData) {
+    setState(() {
+      _filters = filterData;
+
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        if (_filters['gluten'] && !meal.isGlutenFree) {
+          return false;
+        }
+        if (_filters['lactose'] && !meal.isLactoseFree) {
+          return false;
+        }
+        if (_filters['vegan'] && !meal.isVegan) {
+          return false;
+        }
+        if (_filters['vegetarian'] && !meal.isVegetarian) {
+          return false;
+        }
+        return true;
+      }).toList();
+    });
+  }
+
+  bool _isMealFavorite(String id) {
+    return _favoriteMeals.any((meal) => meal.id == id);
+  }
+
+  void _toggleFavorite(String mealId) {
+    final existingIndex =
+        _favoriteMeals.indexWhere((meal) => meal.id == mealId);
+    if (existingIndex >= 0) {
+      setState(() {
+        _favoriteMeals.removeAt(existingIndex);
+      });
+    } else {
+      setState(() {
+        _favoriteMeals.add(DUMMY_MEALS.firstWhere((meal) => meal.id == mealId));
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,29 +77,44 @@ class MyApp extends StatelessWidget {
       builder: DevicePreview.appBuilder,
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-    @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-      ),
-      body: Center(
-        child: Text('Navigation Time!'),
-      ),
+          appBarTheme: AppBarTheme(
+            backgroundColor: Color.fromRGBO(0, 0, 0, 1),
+          ),
+          primaryColor: Color.fromRGBO(0, 0, 0, 1),
+          accentColor: Color.fromARGB(255, 91, 185, 95),
+          canvasColor: Color.fromRGBO(40, 39, 39, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(color: Color.fromRGBO(255, 255, 255, 1)),
+              bodyText2: TextStyle(color: Color.fromRGBO(252, 242, 230, 1)),
+              headline4:
+                  TextStyle(color: Color.fromRGBO(40, 39, 39, 1), fontSize: 15),
+              headline6: TextStyle(
+                  fontFamily: 'RobotoCondensed',
+                  fontSize: 20,
+                  color: Colors.white),
+              headline3: TextStyle(
+                  fontFamily: 'RobotoCondensed',
+                  fontSize: 20,
+                  color: Colors.black),
+              headline5:
+                  TextStyle(fontFamily: 'RobotoCondensed', fontSize: 20))),
+      // home: CategoriesScreen(),
+      routes: {
+        '/': (ctx) => TabsScreen(_favoriteMeals),
+        CategoryMealsScreen.routeName: (ctx) =>
+            CategoryMealsScreen(_availableMeals),
+        MealDetailsScreen.routeName: (ctx) =>
+            MealDetailsScreen(_toggleFavorite, _isMealFavorite),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(_filters, _setFilters),
+      },
+      // onGenerateRoute: (settings) {
+      //   print(settings.arguments);
+      //   return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      // },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
     );
   }
 }
---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import '../screens/filters_screen.dart';
+
+class MainDrawer extends StatelessWidget {
+  // const MainDrawer({ Key? key }) : super(key: key);
+  Widget buildListTile(String title, IconData icon, Function tapHandler) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+        color: Colors.white,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: tapHandler,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(children: [
+        Container(
+          height: 150,
+          width: double.infinity,
+          padding: EdgeInsets.all(20),
+          alignment: Alignment.centerLeft,
+          color: Theme.of(context).accentColor,
+          child: Text(
+            'Cooking Up!',
+            style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 26,
+                color: Theme.of(context).primaryColor),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        buildListTile('Meals', Icons.restaurant, () {
+          Navigator.of(context).pushReplacementNamed('/');
+        }),
+        buildListTile('Filters', Icons.settings_outlined, () {
+          Navigator.of(context).pushReplacementNamed(FiltersScreen.routeName);
+        }),
+      ]),
+    );
+  }
+}
